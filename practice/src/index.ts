b53767561adf0ccb5e77f59415d802d2d5921e26
maton369 <ysk901085@gmail.com>
2025-08-01 11:25:41 +0900

    Add: TSの実行環境を整備

---
+++ practice/src/index.ts
@@ -0,0 +1,8 @@
+// `message` という名前の定数を定義する。
+// 型注釈 `: string` により、この定数が文字列型であることを明示している。
+// 値 `"Hello, world!"` を代入して初期化している。
+const message: string = "Hello, world!";
+
+// `console.log()` 関数を使って、`message` の内容を標準出力（コンソール）に表示する。
+// ここでは "Hello, world!" が出力される。
+console.log(message);
